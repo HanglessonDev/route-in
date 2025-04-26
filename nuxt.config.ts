b7d2c888726fbbe05df9733ee2596d53518bac46
Hanglesson <hanglesson.dev@gmail.com>
2025-04-26 14:57:23 -0500

    chore: remove unused dependencies and add biomejs for improved linting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,61 +1,61 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
-  ssr: false,
+	compatibilityDate: '2024-11-01',
+	devtools: { enabled: true },
+	ssr: false,
 
-  modules: [
-    '@nuxt/eslint',
-    '@nuxt/fonts',
-    '@nuxt/icon',
-    '@nuxt/image',
-    '@nuxt/test-utils',
-    'vuetify-nuxt-module',
-    '@pinia/nuxt',
-    'pinia-plugin-persistedstate/nuxt',
-    '@nuxtjs/tailwindcss',
-    '@vueuse/nuxt',
-  ],
-  plugins: ['~/plugins/dexie.client.ts'],
-  app: {
-    head: {
-      title: 'Route-in App',
-      htmlAttrs: {
-        lang: 'pt-BR',
-      },
-    },
-  },
-  runtimeConfig: {
-    hereApiKey: process.env.HERE_API_KEY, // Accessible only on the server
-    public: {
-      hereApiKey: process.env.HERE_API_KEY,
-    },
-  },
-  tailwindcss: {
-    cssPath: ['~/assets/css/tailwind.css', { injectPosition: 'last' }],
-    // configPath: 'tailwind.config',
-    exposeConfig: true,
-    viewer: true,
-  },
-  vuetify: {
-    vuetifyOptions: {
-      theme: {
-        defaultTheme: 'dark',
-      },
-      defaults: {
-        VTextField: {
-          variant: 'underlined',
-          density: 'compact',
-        },
-        VSelect: {
-          variant: 'underlined',
-          density: 'compact',
-        },
-        VAutocomplete: {
-          variant: 'underlined',
-          density: 'compact',
-        },
-      },
-    },
-  },
-});
+	modules: [
+		'@nuxt/fonts',
+		'@nuxt/icon',
+		'@nuxt/image',
+		'@nuxt/test-utils',
+		'vuetify-nuxt-module',
+		'@pinia/nuxt',
+		'pinia-plugin-persistedstate/nuxt',
+		'@nuxtjs/tailwindcss',
+		'@vueuse/nuxt',
+	],
+	components: [{ path: '~/modules/offlineDB/components', pathPrefix: false }],
+	plugins: ['~/plugins/dexie.client.ts'],
+	app: {
+		head: {
+			title: 'Route-in App',
+			htmlAttrs: {
+				lang: 'pt-BR',
+			},
+		},
+	},
+	runtimeConfig: {
+		hereApiKey: process.env.HERE_API_KEY, // Accessible only on the server
+		public: {
+			hereApiKey: process.env.HERE_API_KEY,
+		},
+	},
+	tailwindcss: {
+		cssPath: ['~/assets/css/tailwind.css', { injectPosition: 'last' }],
+		// configPath: 'tailwind.config',
+		exposeConfig: true,
+		viewer: true,
+	},
+	vuetify: {
+		vuetifyOptions: {
+			theme: {
+				defaultTheme: 'dark',
+			},
+			defaults: {
+				VTextField: {
+					variant: 'underlined',
+					density: 'compact',
+				},
+				VSelect: {
+					variant: 'underlined',
+					density: 'compact',
+				},
+				VAutocomplete: {
+					variant: 'underlined',
+					density: 'compact',
+				},
+			},
+		},
+	},
+})
